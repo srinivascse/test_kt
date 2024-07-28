e6b1fefbcc188dc8c79c7bc6f30b167e6520cd70
srinivascse <34503941+srinivascse@users.noreply.github.com>
2024-07-28 21:54:31 +0530

    latest change

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),
